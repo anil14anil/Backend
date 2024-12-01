ea173825716f381e732c3ff4082df593790b10b9
anil14anil <anil14anil@gmail.com>
2024-12-01 23:24:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+// const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/facebook', (req, res) => {
+  res.send('<h1>Hello Facebook!</h1>')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h2>Please enter with login code</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
