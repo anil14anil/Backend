ed8618cbb452e6ca78d41a36f75eca7c076e589c
anil14anil <anil14anil@gmail.com>
2024-12-02 01:20:54 +0530

    console removed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 // const port = 3000
-console.log("add")
+// console.log("add")
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
